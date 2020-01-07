e32a736fe819a102e9a6931ae97b07ddb974ea30
Robert Boros <robbie13.boros@gmail.com>
2020-01-07 21:17:28 +0100

    adding more comments

--- src/main/java/hu/flowacademy/eta/demo/tvshow/TvShow.java
+++ src/main/java/hu/flowacademy/eta/demo/tvshow/TvShow.java
@@ -3,14 +3,36 @@ package hu.flowacademy.eta.demo.tvshow;
 import javax.persistence.*;
 import java.util.UUID;
 
+/**
+ * TvShow contains a tv show's data
+ * <br>
+ * &#64;Entity - shows that TvShow class represents a database table, <b>required</b> in JPA definition
+ * <br>
+ * &#64;Table - helps us to do a more specific definition for the database table
+ */
 @Entity
 @Table
 public class TvShow {
 
+    /**
+     * The id column of the table
+     * <br>
+     * &#64;Id - shows that this attribute is the primary key of the table <b>required</b>
+     * <br>
+     * &#64;GeneratedValue - define auto increment for this column
+     * <br>
+     * &#64;SequenceGenerator - define sequence parameters
+     */
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tv_show_seq")
+    @SequenceGenerator(name = "tv_show_seq", sequenceName = "tv_show_seq", allocationSize = 1, initialValue = 1)
     private UUID id;
 
+    /**
+     * An example for column definition
+     * <br>
+     * &#64;Column - define the column detailsw
+     */
     @Column(length = 50)
     private String showrunnerName;
 
--- src/main/java/hu/flowacademy/eta/demo/tvshow/TvShowJPARepository.java
+++ src/main/java/hu/flowacademy/eta/demo/tvshow/TvShowJPARepository.java
@@ -8,11 +8,30 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * TvShowJPARepository extends the JpaRepository<{entity class},{type of the entity class's id}>
+ * <br>
+ * &#64;Repository - Special component for data storage
+ */
 @Repository
 public interface TvShowJPARepository extends JpaRepository<TvShow, UUID> {
 
+    /**
+     * <b>findByTitle</b> represents a SQL query something like<br><pre>SELECT * FROM tv_show WHERE title=?1</pre>
+     * <br>
+     *
+     * @param title input value of the where condition
+     * @return List&#60;TvShow&#62; a result of the query
+     */
     List<TvShow> findByTitle(String title);
 
+    /**
+     * <b>findCustomQuery</b> represents a custom HQL query which is the default param of the &#64;Query annotation
+     * <br>
+     *
+     * @param s input value of the where condition, number of the seasons
+     * @return List&#60;TvShow&#62; a result of the query
+     */
     @Query("from TvShow t where t.length > 20 AND t.length < 190 AND t.seasons = :seasonNum")
     List<TvShow> findCustomQuery(@Param("seasonNum") int s);
 
