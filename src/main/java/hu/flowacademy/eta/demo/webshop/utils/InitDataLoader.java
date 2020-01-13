923b4975c8e1751d3b2d6acb94d5f621c4aefd09
Karoly Pakozdi <Karoly.Pakozdi@logmein.com>
2020-01-13 20:06:37 +0100

    Adding fake data.

--- src/main/java/hu/flowacademy/eta/demo/webshop/utils/InitDataLoader.java
+++ src/main/java/hu/flowacademy/eta/demo/webshop/utils/InitDataLoader.java
@@ -1,50 +1,66 @@
 package hu.flowacademy.eta.demo.webshop.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import com.mysql.cj.log.LogFactory;
+import hu.flowacademy.eta.demo.timeoff.model.AvailableHolidays;
+import hu.flowacademy.eta.demo.timeoff.model.User;
+import hu.flowacademy.eta.demo.timeoff.repository.UserRepository;
 import hu.flowacademy.eta.demo.webshop.models.Currency;
 import hu.flowacademy.eta.demo.webshop.models.Product;
-import hu.flowacademy.eta.demo.webshop.models.ShippingAddress;
 import hu.flowacademy.eta.demo.webshop.repository.CurrencyRepository;
 import hu.flowacademy.eta.demo.webshop.repository.OrderRepository;
 import hu.flowacademy.eta.demo.webshop.repository.ProductRepository;
 import hu.flowacademy.eta.demo.webshop.repository.ShippingAddressRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 @Transactional
 @Slf4j
+@AllArgsConstructor
 public class InitDataLoader {
 
     private final CurrencyRepository currencyRepository;
     private final OrderRepository orderRepository;
     private final ProductRepository productRepository;
     private final ShippingAddressRepository shippingAddressRepository;
-
-    public InitDataLoader(CurrencyRepository currencyRepository, OrderRepository orderRepository, ProductRepository productRepository, ShippingAddressRepository shippingAddressRepository) {
-        this.currencyRepository = currencyRepository;
-        this.orderRepository = orderRepository;
-        this.productRepository = productRepository;
-        this.shippingAddressRepository = shippingAddressRepository;
-    }
+    private final UserRepository userRepository;
 
     @PostConstruct
     public void init() {
         loadCurrencies();
         loadProducts();
+        loadUsersAndAvailableHolidays();
+    }
+
+    private void loadUsersAndAvailableHolidays() {
+        // Create fake objects
+        User u = new User();
+        u.setUsername("root");
+        u.setRole("ADMIN");
+        u.setBoss(null);
+
+        AvailableHolidays rootHolidays = new AvailableHolidays();
+        //rootHolidays.setCount(Integer.MAX_VALUE);
+
+        // Bind user and holidays together
+        rootHolidays.setUser(u);
+        u.setAvailableHolidays(rootHolidays);
+
+        userRepository.save(u);
     }
 
     public void loadCurrencies() {
@@ -65,10 +81,14 @@ public class InitDataLoader {
 
     private List<String> getLines(String s) {
         try {
-            return Files.readAllLines(new File("src/main/resources/mocks/" + s).toPath());
+            return Files.readAllLines(getFile(s).toPath());
         } catch (IOException e) {
             log.error("missing file: {}", s);
             return List.of();
         }
+    }
+
+    private File getFile(String file) {
+        return new File("src/main/resources/mocks/" + file);
     }
 }
